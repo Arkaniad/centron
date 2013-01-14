9374375a92a67b9ebd9609a967655bda819c3dbf
Tanner Danzey <arkaniad@gmail.com>
2013-01-13 23:05:49 -0600

    Vaguely more engine-like. I like.

--- src/centron.cpp
+++ src/centron.cpp
@@ -11,104 +11,87 @@ const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP    = 32;
 
-//Loading Function
+SDL_Surface *image  = NULL;
+SDL_Surface *screen = NULL;
+
+SDL_Event event;
+
+std::string tag = "[Centron] ";
+
+void log(std::string message){
+  std::cout << tag << message << std::endl;
+}
 SDL_Surface *load_image(std::string filename){
-  //Temporarily store image
+  log("Loading image "+filename);
   SDL_Surface* loadedImage    = NULL;
-  //Optimized image
   SDL_Surface* optimizedImage = NULL;
-  
-  //Load the image!
   loadedImage = IMG_Load(filename.c_str());
-  
-  //Check image and optimize
   if(loadedImage != NULL){
-    //Optimize
+    log("Successfully loaded "+filename);
     optimizedImage = SDL_DisplayFormat(loadedImage);
-    //Free the old image
     SDL_FreeSurface(loadedImage);
   }
-  
-  //Return the good stuff!
   return optimizedImage;
 }
-
-//Blitting function
 void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* dest){
-    //Make a temporary rect to hold the offset;
+    log("Blitting surface");
     SDL_Rect offset;
     offset.x = x;
     offset.y = y;
-    
-    //Go forth and blit
     SDL_BlitSurface(source, NULL, dest, &offset);
 }
-
-int main(int argc, const char* args[]){
-  SDL_Surface *screen;
-  SDL_Surface *message;
-  SDL_Surface *background;
-  
+bool init(){
+  log("Initializing Engine");
   //Initialize all SDL subsystems
   if(SDL_Init(SDL_INIT_EVERYTHING) == -1){
-    return 1;
+    log("Failed to init SDL.");
+    return false;
   }
   
   //Set up screen
   screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
   if(screen == NULL){
-    return 1;
+    log("Failed to init screen.");
+    return false;
   }
   
   SDL_WM_SetCaption("Centron", NULL);
-  
-  //Load images
-  message     = load_image("hello.bmp");
-  background  = load_image("background.bmp");
-  
-  //Apply background
-  apply_surface(0, 0, background, screen);
-  apply_surface(320, 0, background, screen);
-  apply_surface(0, 240, background, screen);
-  apply_surface(320, 240, background, screen);
-  
-  apply_surface(180, 140, message, screen);
-  
-  //Update screen
-  if(SDL_Flip(screen) == -1){
+  return true;
+}
+bool load_files(){
+  log("Loading content");
+  image = load_image("x.png");
+  if(image == NULL){
+    return false;
+  }
+  return true;
+}
+void clean_up(){
+  log("Cleaning up");
+  SDL_FreeSurface(image);
+  SDL_Quit();
+}
+
+int main(int argc, const char* args[]){
+  bool quit = false;
+  if(!init()){
+    return 1;
+  }
+  if(load_files() == false){
+    return 1;
+  }  
+  apply_surface(0,0,image,screen);
+  if(SDL_Flip(screen)==-1){
     return 1;
   }
   
-  
-  //Wait
-  SDL_Delay(2000);
-  
-  //Loop and move silly ways
-  int x_move = 180;
-  int y_move = 140;
-  bool loop = true;
-  do {
-    apply_surface(0, 0, background, screen);
-    apply_surface(320, 0, background, screen);
-    apply_surface(0, 240, background, screen);
-    apply_surface(320, 240, background, screen);
-    x_move++;
-    y_move++;
-    apply_surface(x_move, y_move, message, screen);
-    if(SDL_Flip(screen) == -1){
-      return 1;
+  while(quit ==false){
+    while(SDL_PollEvent(&event)){
+      if(event.type == SDL_QUIT){
+        quit = true;
+      }
     }
-    SDL_Delay(10);
-    
-  } while(loop);
-  
-  //Free
-  SDL_FreeSurface(message);
-  SDL_FreeSurface(background);
-  
-  //Quit
-  SDL_Quit();
-  
-  //Return
+  }
+  clean_up();
   return 0;
 }
