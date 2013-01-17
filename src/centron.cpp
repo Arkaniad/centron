570cc669da13ab301f077c28178e5b81776a35ac
Tanner Danzey <arkaniad@gmail.com>
2013-01-17 00:22:17 -0600

    Cute little sprite smiley!

--- src/centron.cpp
+++ src/centron.cpp
@@ -15,7 +15,11 @@ const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP    = 32;
 
-SDL_Surface *image  = NULL;
+SDL_Surface *sprite  = NULL;
+SDL_Rect clip[4];
+//Set the clip box
+
+
 SDL_Surface *background = NULL;
 SDL_Surface *screen = NULL;
 
@@ -43,12 +47,12 @@ SDL_Surface *load_image(std::string filename){
   }
   return optimizedImage;
 }
-void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* dest){
+void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* dest, SDL_Rect* clip = NULL){
     log.info("Blitting surface");
     SDL_Rect offset;
     offset.x = x;
     offset.y = y;
-    SDL_BlitSurface(source, NULL, dest, &offset);
+    SDL_BlitSurface(source, clip, dest, &offset);
 }
 bool init(){
   log = Logger(tag);
@@ -71,16 +75,36 @@ bool init(){
 }
 bool load_files(){
   log.info("Loading content");
-  image = load_image("smile.bmp");
+  sprite = load_image("smileanim.bmp");
+  clip[0].x = 0;
+  clip[0].y = 0;
+  clip[0].w = 128;
+  clip[0].h = 128;
+  
+  clip[1].x = 128;
+  clip[1].y = 0;
+  clip[1].w = 128;
+  clip[1].h = 128;
+  
+  clip[2].x = 0;
+  clip[2].y = 128;
+  clip[2].w = 128;
+  clip[2].h = 128;
+  
+  clip[3].x = 128;
+  clip[3].y = 128;
+  clip[3].w = 128;
+  clip[3].h = 128;
+  
   background = load_image("background.bmp");
-  if(image == NULL){
+  if(sprite == NULL){
     return false;
   }
   return true;
 }
 void clean_up(){
   log.info("Cleaning up");
-  SDL_FreeSurface(image);
+  SDL_FreeSurface(sprite);
   SDL_Quit();
 }
 
@@ -92,8 +116,11 @@ int main(int argc, const char* args[]){
   if(load_files() == false){
     return 1;
   }  
+  
+  
   apply_surface(0,0,background,screen);
-  apply_surface(64,324,image,screen);
+  apply_surface(64,324,sprite,screen,&clip[0]);
+  apply_surface(256,324,sprite,screen,&clip[1]);
   if(SDL_Flip(screen)==-1){
     return 1;
   }
