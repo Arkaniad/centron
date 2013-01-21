b3f221a2ae4c63b206a7392172ef3e79c5d06d6f
Tanner Danzey <arkaniad@gmail.com>
2013-01-20 21:28:37 -0600

    Forgot some files. Whoops.

---
+++ src/lib/gui/buttonstates.cpp
@@ -0,0 +1,2 @@
+#include "buttonstates.h"
+
---
+++ src/lib/gui/buttonstates.h
@@ -0,0 +1,12 @@
+#ifndef BUTTONSTATES_H
+#define BUTTONSTATES_H
+
+namespace Centron{
+  namespace GUI{
+    enum ButtonStates{
+      CLIP_MOUSEOVER, CLIP_MOUSEDOWN, CLIP_MOUSEUP, CLIP_MOUSEOUT
+    };
+  }
+}
+
+#endif // BUTTONSTATES_H
